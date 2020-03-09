1d0268bcea2f141ab84c369304d630fb780ae22d
jely2002 <jw.glebbeek@gmail.com>
2020-03-09 11:15:30 +0100

    Plugin rewrite

---
+++ src/main/java/com/jelleglebbeek/antirain/EventListener.java
@@ -0,0 +1,38 @@
+package com.jelleglebbeek.antirain;
+
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.weather.WeatherChangeEvent;
+
+import java.util.Objects;
+
+public class EventListener implements Listener {
+
+    private Main main;
+
+    EventListener(Main main) {
+        this.main = main;
+    }
+
+    @EventHandler
+    public void onRain(WeatherChangeEvent e) {
+        if(e.toWeatherState()) {
+            if(main.getConfig().getStringList("disabled-worlds").contains(e.getWorld().getName().toLowerCase()) || main.getConfig().getStringList("disabled-worlds").contains(e.getWorld().getName())) {
+                return;
+            } else {
+                e.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void command(PlayerCommandPreprocessEvent e) {
+        if(e.getMessage().startsWith("/weather")) {
+            if(e.getPlayer().hasPermission("ar.override")) return;
+            e.setCancelled(true);
+            e.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(main.getConfig().getString("disable-weather-message"))));
+        }
+    }
+}
---
+++ src/main/java/com/jelleglebbeek/antirain/Main.java
@@ -0,0 +1,47 @@
+package com.jelleglebbeek.antirain;
+
+import org.bukkit.ChatColor;
+import org.bukkit.World;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().log(Level.INFO, ChatColor.BLUE + "AntiRain by jely2002 has been initialized");
+        checkUpdate();
+        checkConfig();
+        initiallyDisableRain();
+        EventListener listener = new EventListener(this);
+        getServer().getPluginManager().registerEvents(listener, this);
+    }
+
+    private void checkConfig() {
+        saveDefaultConfig();
+        //Method reserved for future config updates
+    }
+
+    private void checkUpdate() {
+        SpigotUpdater updater = new SpigotUpdater(this, 27224);
+        try {
+            if (updater.checkForUpdates())
+                getLogger().log(Level.INFO, "An update for AntiRain was found! New version: " + updater.getLatestVersion() + " download: " + updater.getResourceURL());
+        } catch (Exception e) {
+            getLogger().log(Level.WARNING, "Could not check for updates! Report this to the author:");
+            e.printStackTrace();
+        }
+    }
+
+    private void initiallyDisableRain() {
+        for(World world : getServer().getWorlds()) {
+            if(getConfig().getStringList("disabled-worlds").contains(world.getName().toLowerCase()) || getConfig().getStringList("disabled-worlds").contains(world.getName())) {
+                return;
+            } else {
+                world.setStorm(false);
+            }
+        }
+    }
+
+}
